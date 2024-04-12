a5271c0c7c6818a9244fc1d6f5985e40664c7076
maxmcd <max.t.mcdonnell@gmail.com>
2024-04-12 10:49:31 -0400

    Add docker and cloudflared

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 const command1 = spawn('node', ['app.js']);
-const command2 = spawn('sleep', ['100000']);
+const command2 = spawn('cloudflared', ['tunnel','--no-autoupdate','run','--token', process.env.CLOUDFLARE_TUNNEL_TOKEN]);
 
 command1.stdout.pipe(process.stdout);
 command1.stderr.pipe(process.stderr);
